c80e9ead210c56acbf35b844c9c8c6a4056ec26d
Alexander Trost <AlexRTrost@gmail.com>
2018-05-12 12:31:04 -0400

    Add basic info-popup for markers

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -5,6 +5,9 @@ function MapMarker(props) {
 
   return (
   <div>
+        <div className="info-popup">
+        "This place has THE BEST, THE BEST, THE BEST, THE BEST REVIEWS."
+    </div>
     <img src={mapMarker} alt="Alt" className="map-marker"/>
         </div>
     )
