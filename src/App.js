339b470ceb10f9d7386efb52403d49b0c0eb31e8
Alexander Trost <AlexRTrost@gmail.com>
2018-05-12 11:04:55 -0400

    Change header to app name

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The Best Reviews</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
