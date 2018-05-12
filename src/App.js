dadacc29cb4a0369c5f1a4705934bbd56aa4f745
Alexander Trost <AlexRTrost@gmail.com>
2018-05-12 11:08:16 -0400

    Add SimpleMap to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import SimpleMap from './components/SimpleMap';
 
 class App extends Component {
   render() {
@@ -8,6 +9,10 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">The Best Reviews</h1>
         </header>
+        <p>
+          I'M ONLY GIVING THE BEST, THE BEST, THE BEST, THE BEST REVIEWS.
+        </p>
+  <SimpleMap />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
