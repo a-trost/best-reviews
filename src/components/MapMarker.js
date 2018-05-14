b651c7056e2cca8a21bb90ee273d795cc49f70ce
Alexander Trost <AlexRTrost@gmail.com>
2018-05-13 20:43:24 -0400

    Change MapMarker to stateful component and add click functions

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import mapMarker from "../img/mapmarker.svg";
 
-function MapMarker(props) {
+class MapMarker extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    super(props);
+    this.state = {
+      isSelected: false
+      };
+  }
 
-  return (
-  <div>
+  handleClick() {
+    this.setState({isSelected:true})
+  }
+
+  handleClose() {
+    this.setState({isSelected:false})
+  }
+
+  infoWindow() {
+      return(
         <div className="info-popup">
         Mystic Aquarium is THE BEST, THE BEST, THE BEST, THE BEST FISH ZOO.
         - Dave Grohl
+        <p onClick={this.handleClose}>X</p>
     </div>
-    <img src={mapMarker} alt="Alt" className="map-marker"/>
+      )
+  }
+
+
+
+  render() {
+  return (
+  <div>
+        {this.state.isSelected && this.infoWindow()}
+    <img src={mapMarker} alt="Alt" className="map-marker" onClick={this.handleClick}/>
         </div>
     )
+  }
   }
 export default MapMarker;
