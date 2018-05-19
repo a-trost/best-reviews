1835909776cd677f82ffb9bef9ed9aac59596af9
Alexander Trost <AlexRTrost@gmail.com>
2018-05-18 20:38:00 -0400

    Change regular textbox to Material UI TextField

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 class SearchBox extends React.Component {
  
   render() {
+    const { classes } = this.props;
    return(
-    <form onSubmit={this.props.handleSubmit}>
-        <label>
-          Enter Zipcode: <input type="text" value={this.props.searchRequest}  onChange={this.props.handleChange} />
-        </label>
+    <form noValidate onSubmit={this.props.handleSubmit} className="search-box-form">
+    <TextField
+          autoFocus
+          className="search-box"
+          id="with-placeholder"
+          label="Enter Zip Code"
+          placeholder="Placeholder"
+          margin="normal"
+          value={this.props.searchRequest}
+          onChange={this.props.handleChange}
+        />
       </form>
 
 
