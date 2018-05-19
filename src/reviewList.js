f79f8b33ed0a70bb8826775c0f50101c0fab7ec0
Alexander Trost <AlexRTrost@gmail.com>
2018-05-18 20:01:46 -0400

    Change lists to strings in review objects

--- src/reviewList.js
+++ src/reviewList.js
@@ -7,119 +7,121 @@ Review object properties and examples:
   connectingPhrase - Connecting verb between "Walmart" and "THE BEST" ("has", "sells", "gives", "makes", "is")
   bestRepetitions - Number of times "THE BEST," is repeated (default: 4)
   endingPhrase - This will be set to Uppercase and include ending punctuation ("KUNG FU.") 
-  categories - For grouping reviews ("shopping", "food", "funPlaces")
+  category - For grouping reviews ("shopping", "food", "funPlaces")
 */
 
-const reviewSource = [
+const reviewlist = [
   {
     desiredResult:"Mystic Aquarium is THE BEST, THE BEST, THE BEST, THE BEST FISH ZOO.",
-    searchTerms:["Aquarium"],
-    precedingPhrase: ""
+    searchTerms:"Aquarium",
+    precedingPhrase: "",
     connectingPhrase:"is",
     bestRepetitions:4,
     endingPhrase:"FISH ZOO.",
-    categories:["funPlaces"]
+    category:"funPlaces"
   },
   {
     desiredResult:"J Crew is THE BEST, THE BEST, THE BEST, THE BEST J CREW.",
-    searchTerms:["J Crew"],
-    precedingPhrase: ""
+    searchTerms:"J Crew",
+    precedingPhrase: "",
     connectingPhrase:"is",
     bestRepetitions:4,
     endingPhrase:"J CREW.",
-    categories:["shopping"]
+    category:"shopping"
   },
   {
     desiredResult:"David's Tailors is THE BEST, THE BEST, THE BEST, THE BEST FOR SUITS",
-    searchTerms:["mens suits"],
-    precedingPhrase: ""
+    searchTerms:"mens suits",
+    precedingPhrase: "",
     connectingPhrase:"is",
     bestRepetitions:4,
     endingPhrase:"FOR SUITS.",
-    categories:["shopping"]
+    category:"shopping"
   },
   {
     desiredResult:" THE BEST, THE BEST, THE BEST, THE BEST ",
-    searchTerms:["pet supplies"],
-    precedingPhrase: ""
+    searchTerms:"pet supplies",
+    precedingPhrase: "",
     connectingPhrase:"stocks",
     bestRepetitions:4,
     endingPhrase:"DOG FOOD.",
-    categories:["shopping"]
+    category:"shopping"
   },
   {
     desiredResult:" THE BEST, THE BEST, THE BEST, THE BEST ",
-    searchTerms:["dairy farm"],
-    precedingPhrase: ""
+    searchTerms:"dairy farm",
+    precedingPhrase: "",
     connectingPhrase:"has",
     bestRepetitions:4,
     endingPhrase:"MOO MOOS.",
-    categories:["funPlaces"]
+    category:"funPlaces"
   },
   {
     desiredResult:" THE BEST, THE BEST, THE BEST, THE BEST ",
-    searchTerms:["private investigator"],
-    precedingPhrase: ""
+    searchTerms:"private investigator",
+    precedingPhrase: "",
     connectingPhrase:"is",
     bestRepetitions:4,
     endingPhrase:"GUMSHOE.",
-    categories:["services"]
+    category:"services"
   },
   {
     desiredResult:" THE BEST, THE BEST, THE BEST, THE BEST ",
-    searchTerms:["airport"],
-    precedingPhrase: ""
+    searchTerms:"airport",
+    precedingPhrase: "",
     connectingPhrase:"has",
     bestRepetitions:4,
     endingPhrase:"PLANE FOOD",
-    categories:["services"]
+    category:"services"
   },
   {
     desiredResult:"Lazer Zone has THE BEST, THE BEST, THE BEST, THE BEST PEW PEW.",
-    searchTerms:["laser tag"],
-    precedingPhrase: ""
+    searchTerms:"laser tag",
+    precedingPhrase: "",
     connectingPhrase:"has",
     bestRepetitions:4,
     endingPhrase:"PEW PEW.",
-    categories:["funPlaces"]
+    category:"funPlaces"
   },
   {
     desiredResult:"Union Station has THE BEST, THE BEST, THE BEST, THE BEST CHOO CHOOS.",
-    searchTerms:["train station"],
-    precedingPhrase: ""
+    searchTerms:"train station",
+    precedingPhrase: "",
     connectingPhrase:"has",
     bestRepetitions:4,
     endingPhrase:"CHOO CHOOS.",
-    categories:["services"]
+    category:"services"
   },
   {
     desiredResult:"Koon Thai makes THE BEST, THE BEST, THE BEST, THE BEST THAI FOOD.",
-    searchTerms:["thai food"],
-    precedingPhrase: ""
+    searchTerms:"thai food",
+    precedingPhrase: "",
     connectingPhrase:"makes",
     bestRepetitions:4,
     endingPhrase:"THAI FOOD",
-    categories:["food"]
+    category:"food"
   },
   {
     desiredResult:"DSW sells THE BEST, THE BEST, THE BEST, THE BEST BLUE SHOES",
-    searchTerms:["shoe store"],
-    precedingPhrase: ""
+    searchTerms:"shoe store",
+    precedingPhrase: "",
     connectingPhrase:"sells",
     bestRepetitions:4,
     endingPhrase:"BLUE SHOES.",
-    categories:["shopping"]
+    category:"shopping"
   }
 ]
+
+export default reviewlist;
 
 /* Empty Object:
   {
     desiredResult:" THE BEST, THE BEST, THE BEST, THE BEST ",
-    searchTerms:[""],
+    searchTerms:"",
     precedingPhrase: ""
     connectingPhrase:"",
     bestRepetitions:4,
     endingPhrase:"",
-    categories:[""]
+    category:""
   },
   */
