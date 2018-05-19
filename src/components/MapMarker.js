2e7421ded2776545d7651658b567e60de249745b
Alexander Trost <AlexRTrost@gmail.com>
2018-05-19 18:37:05 -0400

    Add star rating system

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import mapMarker from "../img/mapmarker.svg";
+import grayStar from "../img/grayStar.svg";
+import goldStar from "../img/goldStar.svg";
 
 class MapMarker extends Component {
   infoWindow() {
       return(
         <div className="info-popup">
       {this.props.bestReview.precedingPhrase}  {this.props.placeName} {this.props.bestReview.connectingPhrase} THE BEST, THE BEST, THE BEST, THE BEST {this.props.bestReview.endingPhrase}
+      {this.starRating(5)}
         <p onClick={this.props.handleCloseFunction}>X</p>
     </div>
       )
+  }
+
+  starRating(rating) {
+    let stars = [grayStar, grayStar, grayStar, grayStar, grayStar];
+    const goldStars = Math.floor(rating);
+    for (let i=0; i<goldStars; i++){
+      stars[i] = goldStar;
+    };
+    return(
+        <div class="stars-outer">
+        {stars.map((star)=><img src={star} width="20px" />)}
+        </div>
+    )
   }
 
   render() {
