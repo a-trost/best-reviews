a18e7f7d1d53adf589dfc3724b109b3b73a1edfb
Alexander Trost <AlexRTrost@gmail.com>
2018-05-19 22:31:08 -0400

    Change star review system to work with Yelp brand guidelines

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import mapMarker from "../img/mapmarker.svg";
-import grayStar from "../img/grayStar.svg";
-import goldStar from "../img/goldStar.svg";
+import yelp0 from "../img/y-0.png";
+import yelp1 from "../img/y-1.png";
+import yelp2 from "../img/y-2.png";
+import yelp3 from "../img/y-3.png";
+import yelp4 from "../img/y-4.png";
+import yelp5 from "../img/y-5.png";
+import dave0 from "../img/d-0.png";
+import dave1 from "../img/d-1.png";
+import dave2 from "../img/d-2.png";
+import dave3 from "../img/d-3.png";
+import dave4 from "../img/d-4.png";
+import dave5 from "../img/d-5.png";
 
 class MapMarker extends Component {
   infoWindow() {
@@ -11,22 +21,24 @@ class MapMarker extends Component {
       {" THE BEST,".repeat(this.props.bestReview.bestRepetitions).slice(0, -1)} 
       {" "} 
       {this.props.bestReview.endingPhrase}
-      {this.starRating(5)}
+      {this.starRating(5, "dave")}
         <p onClick={this.props.handleCloseFunction}>X</p>
     </div>
       )
   }
 
-  starRating(rating) {
-    let stars = [grayStar, grayStar, grayStar, grayStar, grayStar];
-    const goldStars = Math.floor(rating);
-    for (let i=0; i<goldStars; i++){
-      stars[i] = goldStar;
-    };
+  starRating(rating, brand) {
+    const starCount = Math.round(rating);
+    let starImage = {}
+    if (brand === "dave") {
+      const daveImages = [dave0, dave1, dave2, dave3, dave4, dave5];
+      starImage = daveImages[starCount];
+    } else {
+      const yelpImages = [yelp0, yelp1, yelp2, yelp3, yelp4, yelp5];
+      starImage = yelpImages[starCount];
+    }
     return(
-        <div class="stars-outer">
-        {stars.map((star)=><img src={star} width="20px" />)}
-        </div>
+        <img src={starImage} alt={`Rating: ${starCount} Stars`}  width="150px"/>
     )
   }
 
