398abf9c5ad00f37cbc64b6df9fcb4e35a74d407
Alexander Trost <AlexRTrost@gmail.com>
2018-05-25 06:05:00 -0400

    Remove react-typist

--- src/components/IntroBox.js
+++ src/components/IntroBox.js
@@ -2,27 +2,10 @@ import React from 'react';
 
 function IntroBox(props) {
   return(
+    <div className="intro-container">
     <div className="intro-box">
-            <Typist 
-          startDelay={500}
-          cursor={{show:false}}
-          >
-   <p className="intro-text-small"> I’ve got another confession to make</p>
-          <Typist.Delay ms={1300} />
-<p className="intro-text-small">I write reviews.</p>
-<Typist.Delay ms={1300} />
-<p className="intro-text-small">Others talk about stuff they hate.</p>
-<Typist.Delay ms={1600} />
-<p className="intro-text-small">Well I’m no fool.</p>
-<Typist.Delay ms={1300} />
-<p className="intro-text-small">They ask me what I dismiss</p>
-<Typist.Delay ms={1300} />
-<p className="intro-text-small">Well I refused.</p>
-<Typist.Delay ms={1600} />
-<p className="intro-text-small">I’m only giving</p>
-<p className="intro-text-large">THE BEST, THE BEST, THE BEST, THE BEST REVIEWS.</p>
-          </Typist>
-          <p>I want the best reviews for 
+    <form noValidate onSubmit={props.handleSubmit} >
+<p>I want the best places for 
 <select className="intro-form intro-form-select" name="category">
   <option value="shopping">&nbsp;Shopping</option>
   <option value="food">&nbsp;Food</option>
