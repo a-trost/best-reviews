0ef832f8557498f571809d05e23dbf3e01df519f
Alexander Trost <AlexRTrost@gmail.com>
2018-06-10 15:46:15 -0400

    Move state to Redux store and mapStateToProps

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,55 +1,58 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import {connect} from "react-redux";
 
-export default class SearchForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchRequest: this.props.searchRequest ? this.props.searchRequest : "",
-      error: ""
-    };
-  }
-  render() {
-    return (
-      <div>
-        <form
-          noValidate
-          onSubmit={this.props.handleSubmit}
-          className="search-box-form"
-        >
-          <div>
+class SearchForm extends Component {
+	render() {
+		return (
+			<div>
+				<form
+					noValidate
+					onSubmit={this.props.handleSubmit}
+					className="search-box-form"
+				>
+					<div>
             I want The Best Reviews for{" "}
-            <select
-              className="intro-form intro-form-select"
-              name="category"
-              id="category"
-              value={this.props.searchCategory}
-              onChange={this.props.handleCategoryChange}
+						<select
+							className="intro-form intro-form-select"
+							name="category"
+							id="category"
+							value={this.props.category}
+							onChange={this.props.handleCategoryChange}
               
-            >
-              <option value="shopping">&nbsp;Shopping</option>
-              <option value="food">&nbsp;Food</option>
-              <option value="funPlaces">&nbsp;Fun</option>
-              <option value="services">&nbsp;Services</option>
-            </select>{" "}
+						>
+							<option value="shopping">&nbsp;Shopping</option>
+							<option value="food">&nbsp;Food</option>
+							<option value="funPlaces">&nbsp;Fun</option>
+							<option value="services">&nbsp;Services</option>
+						</select>{" "}
             in{" "}
-            <TextField
-              autoFocus
-              className="search-box"
-              id="with-placeholder"
-              label="Enter Zip Code"
-              placeholder="Placeholder"
-              margin="normal"
-              value={this.props.searchRequest}
-              onChange={this.props.handleSearchBoxChange}
-            />{" "}
+						<TextField
+							autoFocus
+							className="search-box"
+							id="with-placeholder"
+							label="Enter Zip Code"
+							placeholder="Placeholder"
+							margin="normal"
+							value={this.props.searchLocation}
+							onChange={this.props.handleSearchBoxChange}
+						/>{" "}
             .<Button variant="raised" color="primary" type="submit">
               Go!
-            </Button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+						</Button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = (state) => { 
+	return { 
+		searchLocation: state.searchLocation, 
+		category: state.category 
+	};
+};
+
+export default connect(mapStateToProps)(SearchForm);
