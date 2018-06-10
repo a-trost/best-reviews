23b3649866e27f7ea067ea81557fa47de5b83366
Alexander Trost <AlexRTrost@gmail.com>
2018-06-10 16:23:07 -0400

    Add VideoPlayer import

--- src/components/SearchResultsPage.js
+++ src/components/SearchResultsPage.js
@@ -4,6 +4,7 @@ import Map from "./Map";
 import SearchForm from "./SearchForm";
 import PlaceList from "./PlaceList";
 import Header from "./Header";
+import VideoPlayer from "./VideoPlayer";
 import {
 	setSelectedMarker,
 	setListFilter,
@@ -12,47 +13,47 @@ import {
 } from "../actions/actions";
 
 class SearchResultsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.clearPlaces = this.clearPlaces.bind(this);
-    this.handleMapMarkerClose = this.handleMapMarkerClose.bind(this);
-    this.handleMapMarkerClick = this.handleMapMarkerClick.bind(this); 
-    this.handleListFilterChange = this.handleListFilterChange.bind(this);
-    this.handleListItemClick = this.handleListItemClick.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.clearPlaces = this.clearPlaces.bind(this);
+		this.handleMapMarkerClose = this.handleMapMarkerClose.bind(this);
+		this.handleMapMarkerClick = this.handleMapMarkerClick.bind(this);
+		this.handleListFilterChange = this.handleListFilterChange.bind(this);
+		this.handleListItemClick = this.handleListItemClick.bind(this);
+	}
 
-  handleMapMarkerClick(event) {
+	handleMapMarkerClick(event) {
 		this.props.dispatch(setSelectedMarker(event.target.id));
-  }
+	}
 
-  handleMapMarkerClose(event) {
+	handleMapMarkerClose(event) {
 		this.props.dispatch(setSelectedMarker());
-  }
+	}
 
-  handleListFilterChange(event) {
+	handleListFilterChange(event) {
 		this.props.dispatch(setListFilter(event.target.value));
-  }
+	}
 
-  handleListItemClick(place, index) {
+	handleListItemClick(place, index) {
 		let lat = place.geometry.location.lat();
 		let lng = place.geometry.location.lng();
 		this.props.dispatch(setSelectedMarker(index));
 		this.props.dispatch(setCenterLatLng(lat, lng));
-  }
+	}
 
-  clearPlaces() {
+	clearPlaces() {
 		this.props.dispatch(clearSearchResults());
-  }
+	}
 
-  getLatLng() {
+	getLatLng() {
 		fetch(
 			`https://maps.googleapis.com/maps/api/geocode/json?address=${
 				this.state.searchRequest
 			}&key=AIzaSyAAsyfic2Tbd2rLhlvIFR0DrUT1MTzzW9M`
 		)
-  .then(function(response) {
-    return response.json();
-  })
+			.then(function(response) {
+				return response.json();
+			})
 			.then(myJson => {
 				return myJson.results[0].geometry.location;
 			})
@@ -60,13 +61,13 @@ class SearchResultsPage extends Component {
 			.then(this.clearPlaces)
 			.then(this.getPlaces)
 			.catch(err => console.error(err));
-  }
+	}
 
-  render() {
-  return (
-    <div className="App">
-      <Header />
-      <SearchForm {...this.props} />
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<SearchForm {...this.props} />
 				<PlaceList
 					{...this.props}
 					handleListFilterChange={this.handleListFilterChange}
