9cb936229f219a67937ef899eecc28674bf93c20
Alexander Trost <AlexRTrost@gmail.com>
2018-06-10 15:37:49 -0400

    Create Redux actions

---
+++ src/actions/actions.js
@@ -0,0 +1,44 @@
+export const setSearchLocation = (searchLocation = "") => ({
+	type: "SET_SEARCH_LOCATION",
+	payload: {
+		searchLocation
+	}
+});
+
+export const setCategory = (category = "") => ({
+	type: "SET_SEARCH_CATEGORY",
+	payload: {
+		category
+	}
+});
+
+export const addSearchResult = (place, bestReview) => ({
+	type: "ADD_SEARCH_RESULT",
+	payload: {
+		place,
+		bestReview
+	}
+});
+
+export const clearSearchResults = () => ({ type: "CLEAR_SEARCH_RESULTS" });
+
+export const setListFilter = (listFilter = "") => ({
+	type: "SET_LIST_FILTER",
+	payload: {
+		listFilter
+	}
+});
+
+export const setSelectedMarker = (markerId = null) => ({
+	type: "SET_SELECTED_MARKER",
+	payload: {
+		markerId
+	}
+});
+
+export const setCenterLatLng = (lat = 41.373346, lng = -71.9532523) => ({
+	type: "SET_CENTER_LAT_LNG",
+	payload: {
+		centerLatLng: { lat, lng }
+	}
+});
