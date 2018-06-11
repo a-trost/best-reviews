45c7aa4150ee878bfdf705de09053c2155387a2f
Alexander Trost <AlexRTrost@gmail.com>
2018-06-11 04:52:29 -0400

    Format with ESLint and Prettier

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import yelp0 from "../img/y-0.png";
 import yelp1 from "../img/y-1.png";
 import yelp2 from "../img/y-2.png";
@@ -12,63 +12,74 @@ import dave3 from "../img/d-3.png";
 import dave4 from "../img/d-4.png";
 import dave5 from "../img/d-5.png";
 import closeImage from "../img/close.png";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 class InfoWindow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      viewDavesReview: true,
-      };
+      viewDavesReview: true
+    };
   }
 
-  createTheBestSpans = (repetitions=4) =>{
-    let spans = []
+  createTheBestSpans = (repetitions = 4) => {
+    let spans = [];
     for (let i = 1; i < repetitions; i++) {
-        spans.push(<span className="the"> THE </span>)
-        spans.push(<span className="best">BEST,</span>)
-        };
-    spans.push(<span className="the"> THE </span>)
-    spans.push(<span className="best">BEST </span>)    
-    return spans
-  }
+      spans.push(<span className="the"> THE </span>);
+      spans.push(<span className="best">BEST,</span>);
+    }
+    spans.push(<span className="the"> THE </span>);
+    spans.push(<span className="best">BEST </span>);
+    return spans;
+  };
 
   davesReview() {
-    return(
+    return (
       <div>
-      <h3>{this.props.placeName}</h3>
-  {this.props.bestReview.precedingPhrase}  {this.props.placeName} 
-  <span className="connecting-phrase"> {this.props.bestReview.connectingPhrase}</span> 
-  {this.createTheBestSpans()}
-  {this.props.bestReview.endingPhrase}
-  <br/>
-  {this.starRating(5, "dave")}
-  <br/>
-  <Button color="primary" variant="outlined" onClick={()=>this.setState({viewDavesReview:false})}>
-       More Reviews
-      </Button>
-  </div>
-    )
+        <h3>{this.props.placeName}</h3>
+        {this.props.bestReview.precedingPhrase} {this.props.placeName}
+        <span className="connecting-phrase">
+          {" "}
+          {this.props.bestReview.connectingPhrase}
+        </span>
+        {this.createTheBestSpans()}
+        {this.props.bestReview.endingPhrase}
+        <br />
+        {this.starRating(5, "dave")}
+        <br />
+        <Button
+          color="primary"
+          variant="outlined"
+          onClick={() => this.setState({ viewDavesReview: false })}
+        >
+          More Reviews
+        </Button>
+      </div>
+    );
   }
 
-  yelpsReview(){
-    return(
-    <div>
-    <h3>A Yelp Review</h3>
-What people on yelp think.
-<br/>
-{this.starRating(3, "yelp")}
-  <br/>
-  <Button color="primary" variant="outlined" onClick={()=>this.setState({viewDavesReview:true})}>
-       Dave's Review
-      </Button>
-</div>
-    )
+  yelpsReview() {
+    return (
+      <div>
+        <h3>A Yelp Review</h3>
+        What people on yelp think.
+        <br />
+        {this.starRating(3, "yelp")}
+        <br />
+        <Button
+          color="primary"
+          variant="outlined"
+          onClick={() => this.setState({ viewDavesReview: true })}
+        >
+          Dave's Review
+        </Button>
+      </div>
+    );
   }
 
   starRating(rating, brand) {
     const starCount = Math.round(rating);
-    let starImage = {}
+    let starImage = {};
     if (brand === "dave") {
       const daveImages = [dave0, dave1, dave2, dave3, dave4, dave5];
       starImage = daveImages[starCount];
@@ -76,19 +87,24 @@ What people on yelp think.
       const yelpImages = [yelp0, yelp1, yelp2, yelp3, yelp4, yelp5];
       starImage = yelpImages[starCount];
     }
-    return(
-        <img src={starImage} alt={`Rating: ${starCount} Stars`}  width="150px"/>
-    )
+    return (
+      <img src={starImage} alt={`Rating: ${starCount} Stars`} width="150px" />
+    );
   }
   render() {
-  return(
-    <div className="info-popup" key={this.props.key}>
-    <img alt="Close Window" src={closeImage} onClick={this.props.handleCloseFunction} className="close" />
-    {this.state.viewDavesReview ? this.davesReview() : this.yelpsReview()}
-    
-</div>
-  )
-}
+    return (
+      <div className="info-popup" key={this.props.key}>
+        <img
+          alt="Close Window"
+          src={closeImage}
+          onClick={this.props.handleCloseFunction}
+          className="close"
+        />
+        {this.state.viewDavesReview ? this.davesReview() : this.yelpsReview()}
+
+      </div>
+    );
+  }
 }
 
 export default InfoWindow;
