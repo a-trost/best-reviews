d91d856379cb636935d9d87663b3d24b67a532a5
Alexander Trost <AlexRTrost@gmail.com>
2018-06-11 04:43:32 -0400

    Format with Prettier and ESLint

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from "react";
 import mapMarker from "../img/mapmarker.svg";
 import InfoWindow from "./InfoWindow";
 
-
 function MapMarker(props) {
-  return(
-  <div>
-        {(props.selectedMarker == props.index) && 
-        <InfoWindow 
-          bestReview={props.bestReview} 
-          placeName={props.placeName}
-          index={props.index}
-          handleCloseFunction={props.handleMapMarkerClose}
-        />
-        }
-    <img src={mapMarker} alt={props.placeName} id={props.index} className="map-marker" onClick={props.handleMapMarkerClick}/>
-        </div>
-    )
-  }
+	return (
+		<div>
+			{props.selectedMarker == props.index && (
+				<InfoWindow
+					bestReview={props.bestReview}
+					placeName={props.placeName}
+					index={props.index}
+					handleCloseFunction={props.handleMapMarkerClose}
+				/>
+			)}
+			<img
+				src={mapMarker}
+				alt={props.placeName}
+				id={props.index}
+				className="map-marker"
+				onClick={props.handleMapMarkerClick}
+			/>
+		</div>
+	);
+}
 export default MapMarker;
