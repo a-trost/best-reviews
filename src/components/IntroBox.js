30410491d8bc708e4fe00b8ff202171dcd420eef
Alexander Trost <AlexRTrost@gmail.com>
2018-06-19 06:44:29 -0400

    Add Dave Grohl portrait to Intro

--- src/components/IntroBox.js
+++ src/components/IntroBox.js
@@ -1,11 +1,15 @@
 import React from "react";
 import SearchForm from "./SearchForm";
 import VideoPlayer from "./VideoPlayer";
+import davePortrait from "../img/DavePortrait.jpg";
+
 
 export default (props) => {
 	return (
 		<div>
 			<h1>This is Dave Grohl.</h1>
+			<img alt="Portrait of Dave Grohl with Guitar" src={davePortrait} />
+			<a href="https://www.flickr.com/photos/track24/8327822421/">Photo Credit</a>
 			<p>
         He's been in some of <span className="body-the-best">the best</span>{" "}
         bands like Nirvana and the Foo Fighters. He's been to all of{" "}
