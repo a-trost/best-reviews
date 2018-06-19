cb09d678ac8ff459a24c7e78e32f7eb6a69f8c71
Alexander Trost <AlexRTrost@gmail.com>
2018-06-19 06:43:56 -0400

    Add Flexbox to Header and create smaller containers for nav icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import backIcon from "../img/backIcon.png";
 
 function Header(props) {
 	return(
-		<header className="App-header">
-			<Link to="/"> <p>Go Back</p> </Link>
-			<h1 className="App-title">The Best Reviews</h1>
+		<header className="header">
+			<div className="header-icons">
+				<Link to="/"> <img src={backIcon} alt="Back Arrow" /> </Link>
+			</div>
+			<div className="header-title-container">
+				<h1 className="header-title">The Best Reviews</h1>
+			</div>
+			<div className="header-icons">
+			</div>
 		</header>
 	);
 }
