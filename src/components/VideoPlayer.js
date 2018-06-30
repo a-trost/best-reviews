6de8339837a5b659c777dddd90c47cf2e8d3ec85
Alexander Trost <AlexRTrost@gmail.com>
2018-06-30 16:41:23 -0400

    Add styles to VideoPlayer and button

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import { animateScroll as scroll } from "react-scroll";
 
 export default class VideoPlayer extends Component {
 	constructor(props) {
@@ -10,6 +12,7 @@ export default class VideoPlayer extends Component {
 	}
 
 	handleVideoToggle() {
+		scroll.scrollToBottom();
 		this.setState(prevState => ({
 			showVideo: !prevState.showVideo
 		}));
@@ -18,20 +21,29 @@ export default class VideoPlayer extends Component {
 	render() {
 		return (
 			<div>
-				<button onClick={this.handleVideoToggle}>
-					{this.state.showVideo ? "Okay, I get it." : "I don't get it."}
-				</button>
+				<Button
+					fullWidth
+					variant="raised"
+					color={this.state.showVideo ? "secondary":"primary" }
+					size="large"
+					onClick={this.handleVideoToggle}
+				>
+					{this.state.showVideo ? "Okay, I get it" : "I don't get it"}
+				</Button>
 				<br />
 				{this.state.showVideo && (
-					<iframe
-						title="Best Of You - Foo Fighters"
-						width="560"
-						height="315"
-						src="https://www.youtube-nocookie.com/embed/h_L4Rixya64?rel=0&amp;start=25"
-						frameBorder="0"
-						allow="autoplay; encrypted-media"
-						allowFullScreen
-					/>
+					<React.Fragment>
+						<br />
+						<iframe
+							title="Best Of You - Foo Fighters"
+							width="100%"
+							height="350"
+							src="https://www.youtube-nocookie.com/embed/h_L4Rixya64?rel=0&amp;start=25"
+							frameBorder="0"
+							allow="autoplay; encrypted-media"
+							allowFullScreen
+						/>
+					</React.Fragment>
 				)}
 			</div>
 		);
